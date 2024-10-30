1bdcf7c9347e38e5bf59b12f8dcc3236ff334399
Hasan Nahiyan Nobel <hasan.nahiyan.nobel@gmail.com>
2024-10-30 23:41:55 +0600

    Completed chapter 14, again again partially.

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -73,14 +73,27 @@ export async function createInvoice(prevState: State, formData: FormData) {
 }
 
 export async function updateInvoice(id: string, formData: FormData) {
-  const {customerId, amount, status} = UpdateInvoice.parse({
+  // Validate form fields using Zod
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
 
-  const amountInCents = amount * 100;
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
 
+  // Prepare data for insertion into the database
+  const {customerId, amount, status} = validatedFields.data;
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  // Insert data into the database
   try {
     await sql`
     UPDATE invoices
