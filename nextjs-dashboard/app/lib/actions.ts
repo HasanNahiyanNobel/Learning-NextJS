e5af4bf85a4a1d4521b660f856acc477bef1beb5
Hasan Nahiyan Nobel <hasan.nahiyan.nobel@gmail.com>
2024-10-31 22:01:52 +0600

    Completed chapter 14, finally.

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -72,38 +72,35 @@ export async function createInvoice(prevState: State, formData: FormData) {
   redirect('dashboard/invoices');
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-  // Validate form fields using Zod
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData,
+) {
   const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
 
-  // If form validation fails, return errors early. Otherwise, continue.
   if (!validatedFields.success) {
     return {
       errors: validatedFields.error.flatten().fieldErrors,
-      message: 'Missing Fields. Failed to Create Invoice.',
+      message: 'Missing Fields. Failed to Update Invoice.',
     };
   }
 
-  // Prepare data for insertion into the database
   const {customerId, amount, status} = validatedFields.data;
   const amountInCents = amount * 100;
-  const date = new Date().toISOString().split('T')[0];
 
-  // Insert data into the database
   try {
     await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
   } catch (error) {
-    return {
-      message: 'Database Error: Failed to Update invoice.',
-    };
+    return {message: 'Database Error: Failed to Update Invoice.'};
   }
 
   revalidatePath('/dashboard/invoices');
