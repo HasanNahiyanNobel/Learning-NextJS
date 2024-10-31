071f3344fffdb9eff91a1e76b3f4f0d60b5a97e7
Hasan Nahiyan Nobel <hasan.nahiyan.nobel@gmail.com>
2024-11-01 01:04:54 +0600

    Completed chapter 15.

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -6,6 +6,8 @@ import {revalidatePath} from 'next/cache';
 import {redirect} from 'next/navigation';
 import {Simulate} from 'react-dom/test-utils';
 import error = Simulate.error;
+import {signIn} from '@/auth';
+import {AuthError} from 'next-auth';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -116,5 +118,24 @@ export async function deleteInvoice(id: string) {
     return {
       message: 'Database Error: Failed to Delete invoice.',
     };
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
   }
 }
