5f2c6fb356f0d25361f44f7c4056fa5db4770e34
Hasan Nahiyan Nobel <hasan.nahiyan.nobel@gmail.com>
2024-11-10 23:02:02 +0600

    Added customisations for Bootstrap.

--- nextjs-play/src/app/layout.js
+++ nextjs-play/src/app/layout.js
@@ -1,4 +1,5 @@
 import {Newsreader} from 'next/font/google';
+import './custom-bootstrap.scss';
 
 const newsreader = Newsreader({
   subsets: ['latin'],
@@ -24,7 +25,7 @@ export default function RootLayout({children}) {
             crossOrigin="anonymous"
         />
       </head>
-      <body className={newsreader.className}>
+      <body className={newsreader.className + ` bg-dark text-light`}>
       {children}
       <script async={true}
               src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
--- nextjs-play/src/app/page.js
+++ nextjs-play/src/app/page.js
@@ -1,7 +1,30 @@
 export default function HomePage() {
   return (
-      <div className="container">
+      <div className="container mt-4">
         <h1>Hey Jude!</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias
+          corporis dicta enim, est ex nobis officia pariatur quisquam
+          reiciendis. Iusto nostrum odit ullam. Aliquid amet aspernatur
+          assumenda cumque deserunt dignissimos doloremque, doloribus dolorum ea
+          eum excepturi expedita facilis fugit id illum inventore ipsa labore
+          laborum minima minus molestias necessitatibus non officia quaerat quam
+          quasi qui quibusdam quo quos ratione reiciendis reprehenderit sapiente
+          sit suscipit veritatis vero voluptate? Animi aperiam dicta eius
+          facere, minima natus quam sequi. Adipisci animi consectetur cumque,
+          dolorem enim, labore possimus quae quam quos sint tenetur vero!
+          Accusantium atque consequuntur dolores ducimus eum exercitationem
+          illum incidunt iure labore laboriosam laborum libero maiores odit,
+          perferendis placeat possimus quisquam reprehenderit! Culpa cupiditate
+          distinctio hic obcaecati optio, placeat vero. Ab aliquid beatae
+          blanditiis consectetur corporis, culpa deleniti dicta dolorem
+          doloribus enim esse est eveniet ex fugit harum id illum impedit
+          inventore ipsam iste maiores minus mollitia necessitatibus nihil nisi
+          numquam odit omnis possimus quae quas quis quo quos recusandae
+          reiciendis rerum sint tenetur! Aliquam cupiditate enim ex ipsa nulla
+          temporibus voluptates. A aliquid beatae dignissimos et, exercitationem
+          fuga ipsam ipsum iusto, magnam molestias natus necessitatibus sunt.
+          Deleniti dignissimos, earum esse id illum impedit ipsa iusto laborum
+          porro quaerat quam quos?</p>
       </div>
   );
 }
